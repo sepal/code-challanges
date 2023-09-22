f183ddb8130cd457f431f19a1778da3ba84033a9
Sebastian Gilits <sep.gil@gmail.com>
2023-09-22 12:23:41 +0200

    Implement python solution

--- 00-fizzbuzz/python/solution.py
+++ 00-fizzbuzz/python/solution.py
@@ -1,2 +1,17 @@
 def fizzbuzz(start: int, end: int) -> list[str]:
-    return []
+    def is_fizz(x): return x % 3 == 0
+    def is_buzz(x): return x % 5 == 0
+    def is_fizzbuzz(x): return is_fizz(x) & is_buzz(x)
+
+    output = []
+    for i in range(start, end + 1):
+        if (is_fizzbuzz(i)):
+            output.append("FizzBuzz")
+        elif is_fizz(i):
+            output.append("Fizz")
+        elif is_buzz(i):
+            output.append("Buzz")
+        else:
+            output.append(str(i))
+
+    return output
