a9eced416025e9a298d58a2596f2a231cafa5199
Sebastian Gilits <sep.gil@gmail.com>
2023-10-04 08:51:13 +0200

    Implement simple python solution

--- 01-add-operators/python/solution.py
+++ 01-add-operators/python/solution.py
@@ -1,4 +1,48 @@
 
+operators = ["*",  "/", "+", "-",]
+
+
+def splitNumber(n: int) -> list[int]:
+    str_n = list(str(n))
+    return [int(x) for x in str_n]
+
+
+def test_operation(digits: list[int], operator: str):
+
+    if operator == "+":
+        res = 0
+        for x in digits:
+            res += x
+        return res
+    elif operator == '-':
+        res = 0
+        for x in digits:
+            res -= x
+        return res
+    elif operator == '*':
+        res = 1
+        for x in digits:
+            res *= x
+        return res
+    elif operator == '*':
+        res = 1
+        for x in digits:
+            res /= x
+        return res
+    return 0
+
 
 def addOperators(source: int, target: int):
-    pass
+    digits = splitNumber(source)
+
+    solutions = []
+    for op in operators:
+        if test_operation(digits, op) == target:
+            str_digits = [str(x) for x in digits]
+            operation_string = op.join(str_digits)
+            solutions.append(operation_string)
+
+    return solutions
+
+
+print(addOperators(123, 6))
